11e32dc85cbe3fbd76dfa1d2b04c322f37dfefbd
pafuentess <pafuentess@unal.edu.co>
2020-07-16 17:01:42 -0500

    first commit

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+def canUnlockAll(boxes):
+    keys = boxes[0]
+    open_box = []
+    for i in range(1, len(boxes)):
+        if i in keys:
+            for j in range(0, len(boxes[i])):
+                if boxes[i][j] == i and boxes[i][j] not in keys:
+                    keys.append(boxes[i][j])
+                else:
+                    if boxes[i][j] not in keys:
+                        keys.append(boxes[i][j])
+    last = keys[len(keys) - 1]
+    for last_keys in boxes[last]:
+        if last_keys not in keys:
+            keys.append(last_keys)
+    if 0 in keys:
+        if len(keys) == len(boxes):
+            return True
+        else:
+            return False
+    else:
+        if (len(keys) + 1 == len(boxes)):
+            return (True)
+        else:
+            return (False)
