3a0e211753b127f68fdb2e6a2c0c0e81f3681661
pafuentess <pafuentess@unal.edu.co>
2020-07-18 16:28:18 -0500

    fix 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,16 +5,15 @@
 def canUnlockAll(boxes):
     """ doc """
     keys = boxes[0]
-    keys.append(0)
     for i in keys:
         for j in boxes[i]:
             index = boxes.index(boxes[i])
-            if index == j and j not in keys:
+            if index == j and j not in keys and j != 0:
                 keys.append(j)
             else:
-                if j not in keys:
+                if j not in keys and j != 0:
                     keys.append(j)
-    if len(keys) == len(boxes):
+    if len(keys) == len(boxes) - 1:
         return True
     else:
         return (False)
