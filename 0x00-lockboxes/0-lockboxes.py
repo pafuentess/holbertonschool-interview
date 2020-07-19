1cccad9140a4c00a3c9fc8b277262852d999ece1
pafuentess <pafuentess@unal.edu.co>
2020-07-19 15:15:42 -0500

    al principio

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,9 +5,8 @@
 def check_initial_keys(boxes, keys):
     k_temp = []
     for i in range(len(keys)):
-        if keys[i] <= len(boxes) and keys[i] not in k_temp:
+        if keys[i] <= len(boxes):
             k_temp.append(keys[i])
-    print(k_temp)
     return (k_temp)
 
 
@@ -25,7 +24,6 @@ def canUnlockAll(boxes):
             for j in boxes[i]:
                 if j not in updated_keys and j < len(boxes):
                     updated_keys.append(j)
-    print(updated_keys)
     if len(updated_keys) == len(boxes):
         return True
     else:
