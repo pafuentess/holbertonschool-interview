2e8cbc5d67632db7b9e7d5b425621ee0e0257d8f
pafuentess <pafuentess@unal.edu.co>
2020-07-19 14:58:44 -0500

    some other cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,26 +5,31 @@
 def check_initial_keys(boxes, keys):
     k_temp = []
     for i in range(len(keys)):
-        if keys[i] <= len(boxes):
-            k_temp.append(keys[i])
+        if keys[i] != 0:
+            if keys[i] < len(boxes) and keys[i] not in k_temp:
+                k_temp.append(keys[i])
     return (k_temp)
 
 
 def canUnlockAll(boxes):
     """ doc """
-    if boxes == [] or len(boxes) == 1:
+    if boxes is None:
+        return False
+    if len(boxes) == 1:
         return True
+    if boxes == []:
+        return False
     if len(boxes[0]) == 0:
         return False
     keys = boxes[0]
-    keys.append(0)
     updated_keys = check_initial_keys(boxes, keys)
+    print(updated_keys)
     for i in updated_keys:
         if i < len(boxes):
             for j in boxes[i]:
-                if j not in updated_keys and j < len(boxes):
+                if j not in updated_keys and j < len(boxes) and j != 0:
                     updated_keys.append(j)
-    if len(updated_keys) == len(boxes):
+    if len(updated_keys) == len(boxes) - 1:
         return True
     else:
         return (False)
