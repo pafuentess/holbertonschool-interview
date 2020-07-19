2f780fe1fee13e100e20668fd6cc53f3fd4b35f0
pafuentess <pafuentess@unal.edu.co>
2020-07-19 15:22:54 -0500

    other cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,8 +5,9 @@
 def check_initial_keys(boxes, keys):
     k_temp = []
     for i in range(len(keys)):
-        if keys[i] <= len(boxes):
+        if keys[i] < len(boxes) and keys[i] not in k_temp:
             k_temp.append(keys[i])
+    print(k_temp)
     return (k_temp)
 
 
