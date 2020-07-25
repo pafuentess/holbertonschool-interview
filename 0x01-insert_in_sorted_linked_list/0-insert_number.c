6a143e19eb1eaea440929788e392332ff8eb058c
pafuentess <pafuentess@unal.edu.co>
2020-07-25 15:27:08 -0500

    tmp null condition

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -21,33 +21,29 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
-	if (*head == NULL)
-		*head = new;
-	else if (tmp1->n > number)
+	if (tmp1 == NULL || tmp1->n > number)
 	{
 		new->next = tmp1;
 		*head = new;
+		return (new);
 	}
-	else
+	while (tmp2->next != NULL)
 	{
-		while (tmp2->next != NULL)
+		if (tmp1->n < number && tmp2->n > number)
 		{
-			if (tmp1->n < number && tmp2->n > number)
-			{
-				tmp1->next = new;
-				new->next = tmp2;
-				break;
-			}
-			else
-			{
-				tmp1 = tmp1->next;
-				tmp2 = tmp2->next;
-			}
+			tmp1->next = new;
+			new->next = tmp2;
+			break;
 		}
-		if (tmp2->next == NULL)
+		else
 		{
-			new = add_nodeint_end(head, number);
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
 		}
+	}
+	if (tmp2->next == NULL)
+	{
+		new = add_nodeint_end(head, number);
 	}
 	return (new);
 }
