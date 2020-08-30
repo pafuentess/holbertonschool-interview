7b6636e4808eb4336f08b158b2a6336afe251d26
pafuentess <pafuentess@unal.edu.co>
2020-08-30 14:12:23 -0500

    fix cast

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-import collections
 
 split = []
 file_size = 0
@@ -14,14 +13,12 @@ try:
         store = split[-2:]
         if store[0] in status:
             status[store[0]] = status[store[0]] + 1
-            num1 = store[1]
-            num2 = num1[:-2]
-            file_size = file_size + int(num2)
+            file_size = file_size + int(store[1])
         if i % 10 == 0:
             print("File size: {}".format(file_size))
             for key in sorted(status.keys()):
                 if status[key] != 0:
-                    print("{}: {}".format(key, status[key]))                  
+                    print("{}: {}".format(key, status[key]))
 finally:
     print("File size: {}".format(file_size))
     for key in sorted(status.keys()):
